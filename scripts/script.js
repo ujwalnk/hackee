30d556318277d1f9df60966a1e6985eefbf62563
Ujwal N K <ujwal.nk@gmail.com>
2023-10-18 21:13:19 +0530

    + Autotype

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,42 @@
+// Dirty fix, add space before and after all text
 var typeText = [
-  "Mark your Calendars for the event",
-  "A 24 hour filled with excitement",
-  "Win awesome prizes"
+  " Mark your Calendars for the event ",
+  " 24 hours filled with excitement ",
+  " Win awesome prizes "
 ]
 
-function autoType(){
-  while(true){
-  for (const text in typeText) {
-    for(const letter in text){
-      document.getElementById("auto_type_div").innerHTML += letter;
-      sleep(2000);
-    }
-    document.getElementById("auto_type_div").innerHTML += "";
+var choosenText = 0;
+var choosenLetter = 0;
+
+var flagSentenceComplete = false;
+var intervalID = setInterval(autoType, 100);
+
+function autoType() {
+  // Clear text if fully printed
+  if (flagSentenceComplete) {
+    document.getElementById("auto_type_div").innerHTML = "";
+    flagSentenceComplete = false;
   }
-}
-}
+  if ((choosenLetter == 0)) {
+    clearInterval(intervalID);
+    intervalID = setInterval(autoType, 2000);
+    flagSentenceComplete = true;
+  } else {
+    clearInterval(intervalID);
+    intervalID = setInterval(autoType, 100);
+    flagSentenceComplete = false;
+  }
+  console.log("Reading", typeText[choosenText][choosenLetter], choosenLetter, choosenText);
+  document.getElementById("auto_type_div").innerHTML += typeText[choosenText][choosenLetter];
+  choosenLetter++;
+  if (choosenLetter == typeText[choosenText].length - 1) {
+    choosenText++;
+    choosenLetter = 0;
+  }
+  if (choosenText == typeText.length) {
+    choosenText = 0;
+  }
+} flagSentenceComplete ? 1000 : 100
 
 // Sleep Function
 function sleep(ms) {
