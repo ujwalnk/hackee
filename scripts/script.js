dcee2cd383f0e64d633b837bf980c193cb66fb15
Sankarshan DH <sankarshan.dh.2003@gmail.com>
2023-10-19 08:40:13 +0530

    Added counter

--- scripts/script.js
+++ scripts/script.js
@@ -42,3 +42,27 @@ function autoType() {
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+// Set the date we're counting down to
+var countDownDate = new Date("NOV 20, 2023 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("counter").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = "EXPIRED";
+  }
+}, 1000);
+  
